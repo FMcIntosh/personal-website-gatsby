3267921da3c62e4ce5f3b1f14ed51b1a8411e2c9
Fraser McIntosh <frasermcintosh1@gmail.com>
2019-02-12 22:34:04 +1300

    Query blogs and projects on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,8 @@ import Layout from '../components/Layout';
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+    const { edges: projects } = data.projects;
+    const { edges: blogPosts } = data.blogPosts;
 
     return (
       <Layout>
@@ -15,7 +16,38 @@ export default class IndexPage extends React.Component {
             <div className="content">
               <h1 className="has-text-weight-bold is-size-2">Latest Projects</h1>
             </div>
-            {posts.map(({ node: post }) => (
+            {projects.map(({ node: post }) => (
+              <div
+                className="content"
+                style={{ border: '1px solid #333', padding: '2em 4em' }}
+                key={post.id}
+              >
+                <p>
+                  <Link className="has-text-primary" to={post.fields.slug}>
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <small>{post.frontmatter.date}</small>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button is-small" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1 className="has-text-weight-bold is-size-2">Latest Posts</h1>
+            </div>
+            {blogPosts.map(({ node: post }) => (
               <div
                 className="content"
                 style={{ border: '1px solid #333', padding: '2em 4em' }}
@@ -55,9 +87,28 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query IndexQuery {
-    allMarkdownRemark(
+    projects: allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] }
       filter: { frontmatter: { templateKey: { eq: "project" } } }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+    blogPosts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
     ) {
       edges {
         node {
