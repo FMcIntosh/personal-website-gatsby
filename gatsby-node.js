732679c97a24bbe0db3215fd79e338627ffb3865
Fraser McIntosh <frasermcintosh1@gmail.com>
2019-04-08 17:50:49 +1200

    create collections node field

--- gatsby-node.js
+++ gatsby-node.js
@@ -75,11 +75,16 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   fmImagesToRelative(node); // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
+    const parent = getNode(_.get(node, 'parent'));
+
+    // Create a field on this node for the "collection" of the parent
+    // NOTE: This is necessary so we can filter `allMarkdownRemark` by
+    // `collection` otherwise there is no way to filter for only markdown
+    // documents of type `post`.
     createNodeField({
-      name: `slug`,
       node,
-      value
+      name: 'collection',
+      value: _.get(parent, 'sourceInstanceName')
     });
   }
 };
