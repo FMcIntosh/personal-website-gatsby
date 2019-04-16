a441b3c64854a8078fba617e47561d3e34f71541
Fraser McIntosh <frasermcintosh1@gmail.com>
2019-04-16 18:57:22 +1200

    Fix typo

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
