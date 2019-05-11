748c1dbe7c61552fd10015be1cbfb0253e6f6381
Fraser McIntosh <frasermcintosh1@gmail.com>
2019-05-11 18:34:57 +1200

    Fix exports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import { DayNightProvider } from './DayNightContex';
 import './src/styles/global.css';
 import NavBar from './src/components/NavBar';
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return <NavBar {...props}>{element}</NavBar>;
