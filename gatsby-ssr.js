dbeb9ced2998a8b0a402446094f2a7f5d3b5dd3b
Fraser McIntosh <frasermcintosh1@gmail.com>
2019-05-11 18:27:06 +1200

    Add navbar back in using gatsby api

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,12 @@ import { DayNightProvider } from './DayNightContex';
 import './src/styles/global.css';
 import NavBar from './src/components/NavBar';
 
+export const wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <NavBar {...props}>{element}</NavBar>;
+};
+
 export const wrapRootElement = ({ element }) => {
   return <DayNightProvider>{element}</DayNightProvider>;
 };
